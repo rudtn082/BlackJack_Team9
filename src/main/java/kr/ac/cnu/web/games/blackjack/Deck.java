0b0273f45bc44c8fac2113c24aca3081f76e6c94
Sim Kyu Young <skq789@gmail.com>
2018-06-17 16:36:30 +0900

    기능추가10. 덱의 남은카드가 10보다 작아지면 새로운 덱을 섞는다.

--- src/main/java/kr/ac/cnu/web/games/blackjack/Deck.java
+++ src/main/java/kr/ac/cnu/web/games/blackjack/Deck.java
@@ -41,6 +41,13 @@ public class Deck {
             // 그래서 보통 게임에서는 N 장의 카드가 남으면 모든 카드를 합쳐서 다시 셔플 합니다.
             // 코드에 그런 내용이 들어가야 함.
             throw new NoMoreCardException();
+
+            // 기능추가 10 . 카드의 숫자가 10보다 작아지면
+            // 새로운 하나의 덱을 섞어서 기존의 덱과 합친다.
+        } else if(cardList.size()<10){
+            Deck newdeck = new Deck(1);
+
+            cardList.addAll(newdeck.cardList);
         }
         return cardList.remove(0);
     }
