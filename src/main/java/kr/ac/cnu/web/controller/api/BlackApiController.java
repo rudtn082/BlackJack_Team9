e42e2b9a9483ac52067bf16047dfc82e1e70bbf7
Sim Kyu Young <skq789@gmail.com>
2018-06-18 18:00:16 +0900

    double down과 current bet 잘 작동되도록 수정

--- src/main/java/kr/ac/cnu/web/controller/api/BlackApiController.java
+++ src/main/java/kr/ac/cnu/web/controller/api/BlackApiController.java
@@ -59,12 +59,12 @@ public class BlackApiController {
 
         GameRoom gameRoom = blackjackService.double_down(roomId, user, betMoney);
 
+        System.out.println("aa");
         // BugFix3
         // 수행된 게임룸에서 플레이어의 balance를 가져옴.
         user.setAccount(gameRoom.getPlayerList().get(name).getBalance());
 
         // 그 값을 저장 userRepository 업데이트
-        System.out.println(user.getAccount());
         userRepository.save(user);
 
 
